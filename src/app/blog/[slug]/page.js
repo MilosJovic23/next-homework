082b98593ff80f8aff5bcdbcd4ac2d6006329b3e
MilosJovic23 <milos.mj992@gmail.com>
2024-04-27 14:30:43 +0200

    * changes to blog\[slug]

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,15 +1,22 @@
 
-import blog from "../../blogPosts.json"
+
+import blogData from "../../blogPosts.json"
+
 
 export default function BlogPosts({ params }) {
 
     return(
         <>
-            { blog.map((blog)=>{
-                if( params.slug===blog.name.toLowerCase() ){
-                    return <p>{blog.name}</p>
-                }
-                <p>that blog post doesnt exist</p>
+            { blogData.map((post)=>{
+
+                    if( post.name.toLowerCase() === params.slug ){
+                       return <p>{ post.description }</p>
+                } else{
+                        return(<p>Specific blog post doesn't exist</p>)
+                    }
+
+
+
             })}
         </>
 
