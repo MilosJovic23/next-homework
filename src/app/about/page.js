85ca29fcf81f60e7cb036febd4c925bfe8e1795c
MilosJovic23 <milos.mj992@gmail.com>
2024-04-29 12:08:19 +0200

    * added async function and called dummyjosn api

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
 
 
-export default function About() {
+export default async function About() {
+
+    const response = await fetch("https://dummyjson.com/posts?limit=5");
+    const data = await response.json();
+
+
+    const post = data["posts"][0]
+
+    console.log(post)
 
     return<>
 
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+        <p>{post.tags}</p>
         <Image src="/images/f1.jpg" alt="forumula1 cover image"
                height="200"
                width="400"
--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,20 +1,32 @@
 
 
 import blogData from "@/BlogPostsJSON/blogPosts.json"
+import Image from "next/image";
 
 
-export default function BlogPosts({ params }) {
+export default async function BlogPosts({ params }) {
+
+    const response = await fetch ('https://dummyjson.com/products/'+params.slug);
+    const data = await response.json();
+
 
     return(
         <>
-            { blogData.map((post)=>{
 
-                    if( post.name.toLowerCase() === params.slug ){
-                       return <p>{ post.description }</p>
-                    }
+            <div>
+                <h1>{data.title}</h1>
+                <p>{data.description}</p>
+                <p>{data.price}</p>
+                <p>{data.category}</p>
+                { data.images.map((img)=>{
+                    return <img style={{width: 200, height:200}} src={img} alt="ds"/>
+                })}
+                <img src={data.images[2]} alt="asd"/>
+            </div>
 
-            })}
+
         </>
+
 
     )
 }
